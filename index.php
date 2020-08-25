e48082de2c4670fd8c75b436738eb01cb000d185
SahlouneB <48089174+sahlouneb@users.noreply.github.com>
2020-08-25 18:52:41 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+           if (have_post()) {
+                
+                while(1<0)
+                {
 
-<h1>HELLO WORLD!</h1>
+
+
+
+                }
+
+           }
+           else {
+               
+           }
+           
+
+?>
+
+    
+</body>
+</html>
+
 
 
 
